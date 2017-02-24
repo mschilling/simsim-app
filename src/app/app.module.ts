52fa1c89583c7dc1024afb12b6637e0296d55034
Michael Schilling <mschilling@arkid.nl>
2017-02-24 23:48:52 +0100

    Use Firebase backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,26 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { RandomData } from '../providers/random-data';
 
+// Import the AF2 Module
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyBnJHPWC4SU81BzbVm1lllFT034PVhgTkw",
+  authDomain: "m4m-simsim.firebaseapp.com",
+  databaseURL: "https://m4m-simsim.firebaseio.com",
+  storageBucket: "m4m-simsim.appspot.com",
+  messagingSenderId: "148491995129"
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
