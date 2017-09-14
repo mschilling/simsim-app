01122b533aef3cf9c7f8e95b8586b97cc66bfa73
Michael Schilling <mschilling@arkid.nl>
2017-09-14 22:38:41 +0200

    Mock some response in /api/open endpoint #4

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,7 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.openGate = functions.https.onRequest((request, response) => {
+  response.send(":eyes:");
+ });
