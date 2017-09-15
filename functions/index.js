f804b75c5dbd6a01ac19292211818bd1f998f8d9
Bart L'Herminez <bartlh@hotmail.nl>
2017-09-15 15:04:14 +0200

    Text & Speech responses + check on openings code

--- functions/index.js
+++ functions/index.js
@@ -17,9 +17,21 @@ exports.openGate = functions.https.onRequest((request, response) => {
   console.log('headers: ' + JSON.stringify(request.headers));
   console.log('body: ' + JSON.stringify(request.body));
 
-  let answer = "This is a sample response from your webhook!" 
+  //get the parameter from the response
+  let Code = request.body.result.parameters['Emotes']; // Emotes is a required parameter(so will never be empty)
+
+  let textAnswer = `whoops, looks like something went wrong`; //default on screen answer
+  let speechAnswer = `whoops, looks like something went wrong`; //default spoken answer
+
+  if(Code == "succes"){
+    textAnswer = `Alright, I'm opening the gate!`;
+    speechAnswer = `Alright. I'm opening the gate`;
+    //do something
+  }else{
+    textAnswer = `Looks like your code didn't work. Please try again`;
+    speechAnswer = `Looks like your code didn't work. Please try again`;
+  }
   
   response.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  response.send(JSON.stringify({ "speech": answer, "displayText": answer 
-  }));
+  response.send(JSON.stringify({ "speech": speechAnswer, "displayText": textAnswer }));
  });
