1f8b48cdf3d43518b09a6fcef1b9a43bb3cffd99
Michael Schilling <mschilling@arkid.nl>
2017-09-16 14:16:12 +0200

    Remove OpenGate HTTP trigger

--- functions/index.js
+++ functions/index.js
@@ -4,22 +4,6 @@ process.env.DEBUG = 'actions-on-google:*';
 const Assistant = require('actions-on-google').ApiAiAssistant;
 const moment = require('moment');
 
-exports.openGate = functions.https.onRequest((request, response) => {
-  const accountSid = functions.config().twilio.sid
-  const authToken = functions.config().twilio.token
-  const twilioNumber = functions.config().twilio.twilio_number
-  const outgoingNumber = functions.config().twilio.gate_number
-  const environment = functions.config().twilio.environment
-
-  const api = new GateKeeperApi(accountSid, authToken, twilioNumber);
-  api.openGate(outgoingNumber, (environment == 'sandbox'))
-    .then(() => response.send(":eyes:"))
-    .catch((err) => {
-      console.log(err);
-      response.send('Error: ' + err);
-    });
-});
-
 exports.assistantWebhook = functions.https.onRequest((request, response) => {
   console.log('headers: ' + JSON.stringify(request.headers));
   console.log('body: ' + JSON.stringify(request.body));
