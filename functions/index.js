36850d48c30d52132c1310ff0d5c84a782b0fadd
Bart L'Herminez <bartlh@hotmail.nl>
2017-09-20 11:28:27 +0200

    SimSim now opens to codes from the database
    added database connection and removed assistant contexts.

--- functions/index.js
+++ functions/index.js
@@ -3,32 +3,48 @@ const GateKeeperApi = require('./helpers/GateKeeperApi');
 
 process.env.DEBUG = 'actions-on-google:*';
 const Assistant = require('actions-on-google').ApiAiAssistant;
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const ref = admin.database().ref('codes');
 
 const moment = require('moment');
 
 exports.assistantWebhook = functions.https.onRequest((request, response) => {
-  console.log('headers: ' + JSON.stringify(request.headers));
-  console.log('body: ' + JSON.stringify(request.body));
+  //console.log('headers: ' + JSON.stringify(request.headers));
+  //console.log('body: ' + JSON.stringify(request.body));
 
-  const assistant = new Assistant({request: request, response: response});
+  const assistant = new Assistant({ request: request, response: response });
 
   //get the parameter from the response
   let passCode = request.body.result.parameters['password']; // passCode is a required parameter(so will never be empty)
-  
-  const code = moment().utcOffset('+02:00').format('HHmm');
-  console.log(`${code}/${passCode}`);
-
-  if (passCode == code) {
-    openGate().then(() => {
-      assistant.setContext("succes");
-      let speech = `<speak> Alright, I'm opening the gate!. </speak>`;
-      assistant.tell(speech);
-      });
-    } else {
-      assistant.setContext("failure"); 
+  //check: 0=false, 1=true, 2=true+debug
+  let check = 0;
+  //loop through the codes in the database and compare them with the user input.
+  ref.once('value', ((data) => {
+    data.forEach((childData) => {
+      let key = childData.key;
+      let debug = childData.val().debug;
+      if (passCode == key) {
+        if (debug == true) {
+          check = 2;
+        } else {
+          check = 1;
+        }
+      }
+    });
+    if (check == 0) {
       let speech = `<speak> Looks like your code didn't work. <break time="1"/> Please try again. </speak>`;
       assistant.ask(speech, ['Please provide the 4 digit code']);
+    } else if (check == 1) {
+      openGate().then(() => {
+        let speech = `<speak> Alright, I'm opening the gate!. </speak>`;
+        assistant.tell(speech);
+      });
+    } else if (check == 2) {
+      let speech = `<speak> Alright, I'm opening the gate, but not for real. </speak>`;
+      assistant.tell(speech);
     }
+  }));
 });
 
 function openGate() {
