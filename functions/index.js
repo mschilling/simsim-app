bf906e0412ad5d04c68fa14d5efd7f26f20e9075
Bart L'Herminez <bartlh@hotmail.nl>
2017-09-20 16:09:21 +0200

    Saves user session of the Google Home

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ const Assistant = require('actions-on-google').ApiAiAssistant;
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const ref = admin.database().ref('codes');
+const userRef = admin.database().ref('session');
 
 const moment = require('moment');
 
@@ -15,12 +16,23 @@ exports.assistantWebhook = functions.https.onRequest((request, response) => {
 
   const assistant = new Assistant({ request: request, response: response });
 
+  //
+  const userId = assistant.getUser().userId;
+  console.log(assistant.getUser() ,userId);
+  //use this code to reset the uid field in the database if it no longer matches.
+  //you should uncomment this line again when the right uid is in the database or it will change the database entry with every new device login.
+  //return userRef.set({uid: userId});  
+  
   //get the parameter from the response
   let passCode = request.body.result.parameters['password']; // passCode is a required parameter(so will never be empty)
+  console.log(passCode);
   //check: 0=false, 1=true, 2=true+debug
   let check = 0;
   //loop through the codes in the database and compare them with the user input.
   ref.once('value', ((data) => {
+    userRef.once('value', ((userData) => {
+      if(userData.val().uid != userId){
+        console.log("unmatched ", userData.val().uid, userId);
     data.forEach((childData) => {
       let key = childData.key;
       let debug = childData.val().debug;
@@ -32,8 +44,12 @@ exports.assistantWebhook = functions.https.onRequest((request, response) => {
         }
       }
     });
-    if (check == 0) {
-      let speech = `<speak> Looks like your code didn't work. <break time="1"/> Please try again. </speak>`;
+  }else if(userData.val().uid == userId){
+    check = 2;
+    console.log("match");
+  }
+  if (check == 0) {
+      let speech = `<speak> Looks like that didn't work. <break time="1"/> Did you use the right code? </speak>`;
       assistant.ask(speech, ['Please provide the 4 digit code']);
     } else if (check == 1) {
       openGate().then(() => {
@@ -45,6 +61,8 @@ exports.assistantWebhook = functions.https.onRequest((request, response) => {
       assistant.tell(speech);
     }
   }));
+    
+}));
 });
 
 function openGate() {
