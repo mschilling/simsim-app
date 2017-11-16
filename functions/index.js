965f261d81c18daa2ead70a6a1a1d99c82510123
Bart L'Herminez <bartlh@hotmail.nl>
2017-11-16 10:59:51 +0100

    SimSim update. Added location check

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ process.env.DEBUG = 'actions-on-google:*';
 const Assistant = require('actions-on-google').ApiAiAssistant;
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+
 const ref = admin.database().ref('codes');
 const userRef = admin.database().ref('session');
 
@@ -16,63 +17,159 @@ exports.assistantWebhook = functions.https.onRequest((request, response) => {
 
   const assistant = new Assistant({ request: request, response: response });
 
-  const userId = assistant.getUser().userId;
-  console.log(assistant.getUser() ,userId);
-  //use this code to reset the uid field in the database if it no longer matches.
-  //you should uncomment this line again when the right uid is in the database or it will change the database entry with every new device login.
-  //return admin.database().ref('session').set({uid2: userId});  
-  
-  //get the parameter from the response
-  let passCode = request.body.result.parameters['password']; // passCode is a required parameter(so will never be empty)
-  console.log(passCode);
-  //check: 0=false, 1=true, 2=true+debug
-  let check = 0;
-  //loop through the codes in the database and compare them with the user input or skip the code and open directly if the user is in the database.
-  ref.once('value', ((data) => {
-    userRef.once('value', ((userData) => {
-      if(userData.val().uid == userId || userData.val().uid1 == userId || userData.val().uid2 == userId 
-      || userData.val().uid3 == userId || userData.val().uid4 == userId){
-        check = 1;
-        console.log("match");
-  }else{
-    console.log("unmatched ", userData.val().uid, userId);
-    data.forEach((childData) => {
-      let key = childData.key;
-      let debug = childData.val().debug;
-      if (passCode == key) {
-        if (debug == true) {
-          check = 2;
-        } else {
-          check = 1;
-        }
-      }
-    });
-  }
-  if (check == 0) {
-      let speech = `<speak> I'll need the right code to open the gate. <break time="1"/> Do you know it? </speak>`;
-      assistant.ask(speech, ['Please provide the 4 digit code']);
-    } else if (check == 1) {
-      openGate().then(() => {
-        let speech = `<speak> Alright, I'm opening the gate!. </speak>`;
+  //actionmap to handle the incoming requests. The name inbetween the quotes matches the action name in api.ai
+  let actionMap = new Map();
+  actionMap.set('input.welcome', welcome);
+  actionMap.set('input.open', open);
+  actionMap.set('input.location', location);
+  assistant.handleRequest(actionMap);
+
+  function location(assistant) {
+    let speech;
+    const userId = assistant.getUser().userId;
+    if (assistant.isPermissionGranted()) {
+      let deviceCoordinates = assistant.getDeviceLocation().coordinates;
+      let displayName = assistant.getUserName().displayName;
+      console.log(displayName);
+      console.log(deviceCoordinates.latitude + "," + deviceCoordinates.longitude);
+
+      let distance = getDistanceFromLatLonInKm(deviceCoordinates.latitude, deviceCoordinates.longitude, 52.508338, 6.0902274);
+
+      //check if the user is within 1km of Move4Mobile in Zwolle
+      //logs all attempts succesfull or not
+      //the userId represents the google account/assistantID
+      if (distance < 1) {
+        admin.database().ref('log/' + userId + '/' + Date.now().toString()).set({
+          user: displayName,
+          lat: deviceCoordinates.latitude,
+          long: deviceCoordinates.longitude
+        });
+        speech = `<speak> Alright, I'm opening the gate, but in debug mode. </speak>`;
         assistant.tell(speech);
-      });
-    } else if (check == 2) {
-      let speech = `<speak> Alright, I'm opening the gate, but not for real. </speak>`;
-      assistant.tell(speech);
+      } else {
+        admin.database().ref('failedAttemps/' + userId + '/' + Date.now().toString()).set({
+          user: displayName,
+          lat: deviceCoordinates.latitude,
+          long: deviceCoordinates.longitude
+        });
+        speech = "You do not have permission to open the gate";
+        assistant.tell(speech);
+      }
     }
-  }));
-    
-}));
+  }
+
+  function welcome(assistant) {
+    const userId = assistant.getUser().userId;
+    let check = 0;
+    ref.once('value', ((data) => {
+      userRef.once('value', ((userData) => {
+        userData.forEach((childData) => {
+          if (childData.val().uid == userId) {
+            check = 1;
+            console.log("match");
+          }
+        })
+        if (check == 1) {
+          openGate().then(() => {
+            let speech = `<speak> Alright, I'm opening the gate!. </speak>`;
+            assistant.tell(speech);
+          });
+        } else {
+          let namePermission = assistant.SupportedPermissions.NAME;
+          let preciseLocationPermission = assistant.SupportedPermissions.DEVICE_PRECISE_LOCATION
+
+          assistant.askForPermissions('To open the gate',
+            [namePermission, preciseLocationPermission]);
+        }
+      }))
+    }))
+  }
+
+
+
+  function open(assistant) {
+    const userId = assistant.getUser().userId;
+    console.log(assistant.getUser(), userId);
+    //use this code to reset the uid field in the database if it no longer matches.
+    //you should uncomment this line again when the right uid is in the database or it will change the database entry with every new device login.
+    //return admin.database().ref('session').set({uid2: userId});  
+
+    //get the parameter from the response
+    let passCode = request.body.result.parameters['password']; // passCode is a required parameter(so will never be empty)
+    console.log(passCode);
+    //check: 0=false, 1=true, 2=true+debug
+    let check = 0;
+    //loop through the codes in the database and compare them with the user input or skip the code and open directly if the user is in the database.
+    ref.once('value', ((data) => {
+      userRef.once('value', ((userData) => {
+        userData.forEach((childData) => {
+          if (childData.val().uid == userId) {
+            check = 1;
+            console.log("match");
+          } else {
+            console.log("unmatched ", userData.val().uid, userId);
+            data.forEach((childData) => {
+              let key = childData.key;
+              let debug = childData.val().debug;
+              if (passCode == key) {
+                if (debug == true) {
+                  check = 2;
+                } else {
+                  check = 1;
+                }
+              }
+            });
+          }
+        })
+        if (check == 0) {
+          let speech = `<speak> I'll need the right code to open the gate. <break time="1"/> Do you know it? </speak>`;
+          assistant.ask(speech, ['Please provide the 4 digit code']);
+        } else if (check == 1) {
+          openGate().then(() => {
+            let speech = `<speak> Alright, I'm opening the gate!. </speak>`;
+            assistant.tell(speech);
+          });
+        } else if (check == 2) {
+          let speech = `<speak> Alright, I'm opening the gate, but in debug mode. </speak>`;
+          assistant.tell(speech);
+        }
+      }));
+
+    }));
+
+  }
+
+  function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+  }
+
+  function openGate() {
+    const accountSid = functions.config().twilio.sid
+    const authToken = functions.config().twilio.token
+    const twilioNumber = functions.config().twilio.twilio_number
+    const outgoingNumber = functions.config().twilio.gate_number
+    const environment = functions.config().twilio.environment
+
+    const api = new GateKeeperApi(accountSid, authToken, twilioNumber);
+    return api.openGate(outgoingNumber, (environment == 'sandbox'))
+
+  }
+
+
 });
 
-function openGate() {
-  const accountSid = functions.config().twilio.sid
-  const authToken = functions.config().twilio.token
-  const twilioNumber = functions.config().twilio.twilio_number
-  const outgoingNumber = functions.config().twilio.gate_number
-  const environment = functions.config().twilio.environment
 
-  const api = new GateKeeperApi(accountSid, authToken, twilioNumber);
-  return api.openGate(outgoingNumber, (environment == 'sandbox'))
-
-}
