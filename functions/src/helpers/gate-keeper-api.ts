b157fb4967685465c528c22e5734205acd247d58
Michael Schilling <mschilling@arkid.nl>
2018-07-20 13:28:29 +0200

    Improve and simplify GateKeeperApi

--- functions/src/helpers/gate-keeper-api.ts
+++ functions/src/helpers/gate-keeper-api.ts
@@ -1,41 +1,43 @@
-
-const twilio = require('twilio');
+import * as twilio from 'twilio';
 
 export class GateKeeperApi {
-  client;
+  client: twilio.RestClient;
 
   constructor(private accountSid, private authToken, private twilioNumber) {
     this.accountSid = accountSid;
     this.authToken = authToken;
     this.twilioNumber = twilioNumber;
-    this.client = new twilio(this.accountSid, this.authToken);
+    this.client = twilio(this.accountSid, this.authToken);
   }
 
   openGate(outgoingNumber, mock = false) {
+    console.log(
+      `openGate(${this.twilioNumber}, ${outgoingNumber}, mock=${mock})`
+    );
 
-    console.log(`openGate(${this.twilioNumber}, ${outgoingNumber}, mock=${mock})`);
-
-    if(mock) {
+    if (mock) {
       console.log('mock open gate!');
-      return Promise.resolve(true)
+      return Promise.resolve(true);
     }
 
     const voiceUrl = 'https://api.michaelschilling.com/voice/default-message';
 
-    return this.client.calls.create({
-      url: voiceUrl,
-      to: outgoingNumber,
-      from: this.twilioNumber,
-      method: 'GET',
-      fallbackMethod: 'GET',
-      statusCallbackMethod: 'GET',
-      record: false,
-      timeout: 5
-
-    }, function (err, message) {
-      if (err) {
-        console.log(err, message.sid);
+    return this.client.calls.create(
+      {
+        url: voiceUrl,
+        to: outgoingNumber,
+        from: this.twilioNumber,
+        method: 'GET',
+        fallbackMethod: 'GET',
+        statusCallbackMethod: 'GET',
+        record: false,
+        timeout: 5,
+      },
+      function(err, message) {
+        if (err) {
+          console.log(err, message.sid);
+        }
       }
-    });
+    );
   }
 }
